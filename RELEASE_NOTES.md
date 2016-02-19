0e907c12ec167005c482946e4ce390d443069e2e
Rick Minerich <richard.minerich@gmail.com>
2016-02-19 17:08:57 -0500

    Updated infos for nuget package.

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,2 @@
-#### 0.0.1-beta - August 5 2015
-* Imported bits from existing internal projects.
-* Initial release
+#### 1.0 - February 19 2016
+* Initial Nuget release
--- src/Fungible/AssemblyInfo.fs
+++ src/Fungible/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Fungible")>]
 [<assembly: AssemblyProductAttribute("Fungible")>]
 [<assembly: AssemblyDescriptionAttribute("A library for fast reflective changes to immutable data trees")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "1.0"
