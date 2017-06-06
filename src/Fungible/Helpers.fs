62deb1c550a8dd30577c0baa7f72d4caf632e1c1
Rick Minerich <richard.minerich@gmail.com>
2017-06-06 18:17:57 -0400

    Some refactoring and beginning of diff framework

---
+++ src/Fungible/Helpers.fs
@@ -0,0 +1,70 @@
+﻿module Fungible.Helpers
+
+open System
+
+open Microsoft.FSharp.Quotations
+
+module ExprHelpers = 
+    /// Shortcut for Expr.Application
+    let inline application prms expr = Expr.Application(expr, prms)
+       
+    /// Shortcut for Expr.Coerce
+    let inline coerse typ expr = Expr.Coerce(expr, typ)
+    /// Shortcut for Expr.NewRecord
+    let inline newrec typ args = Expr.NewRecord(typ, args)
+    /// Shortcut for Expr.Call    
+    let inline call meth args = Expr.Call(meth, args)
+
+    /// Makes it easy to get a MethodInfo out of a quotation. 
+    /// x: let m = (getMethod <@ failwith message @>).MakeGenericMethod([|utype|])
+    let getMethod = 
+        function
+        | Patterns.Call (_, m, _) when m.IsGenericMethod -> m.GetGenericMethodDefinition()
+        | Patterns.Call (_, m, _) -> m
+        | _ -> failwith "Incorrect getMethod Pattern"
+
+    /// Makes it easy to get a placeholder value for a type used to be used in a quotation
+    let X<'T> : 'T = Unchecked.defaultof<'T>
+
+    /// Compare Application
+    let returnNotUnitSecond (_: 'a) (v2: 'b) = v2
+    let inline applyCompare (prmType: Type) (name: string list) prm1 prm2 compFunExpr = 
+        let inputs = Expr.NewTuple [Expr.Value (List.rev name); prm1; prm2]
+        let app = Expr.Application(compFunExpr, inputs)
+        let meth = (getMethod <@ returnNotUnitSecond X X @>).MakeGenericMethod([|typeof<unit>; prmType|])
+        Expr.Call(meth, [app; prm2])
+
+module CollectionHelpers =
+
+    let internal defaultOptionIfNull defaultfun option = if Option.isNone option then defaultfun() else option
+
+    let internal defaultArrayIfEmpty defaultfun arr = if Array.isEmpty arr then defaultfun() else arr
+    let internal addToArray addFun arr = 
+        let r : _ [] = addFun ()
+        if r.Length > 0 then Array.append arr r
+        else arr
+
+    let internal mapMapValues (mapFun: ('k*'v) -> ('k*'v)) (m : Map<'k,'v>) : Map<'k,'v> = 
+        m |> Map.toSeq |> Seq.map mapFun |> Map.ofSeq
+
+    let internal filterMap (filterFun: ('k * 'v) -> bool) (m: Map<'k,'v>) = 
+         m |> Map.filter (fun k v -> filterFun (k, v))
+
+    let internal filterOption (f: 'a -> bool) = 
+        function
+        | Some v when f v -> Some v
+        | Some _ | None -> None
+
+    let internal defaultMapIfEmpty (defaultfun: unit -> Map<'k,'v>) m = 
+        if Map.isEmpty m then defaultfun() else m
+
+    let internal addMap (addFun: unit -> Map<'k,'v>) m = 
+        addFun() |> Map.fold (fun st k v -> st |> Map.add k v) m 
+
+    let (|IsMapType|_|) (t: Type) = 
+        if t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<Map<_,_>> then Some t
+        else None    
+
+    let (|IsOptionType|_|) (t: Type) =
+        if t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<_>> then Some t
+        else None
---
+++ src/Fungible/Walker.fs
@@ -0,0 +1,55 @@
+﻿module Fungible.Walker
+
+open Fungible.Helpers
+open Fungible.Helpers.ExprHelpers
+open Fungible.Helpers.CollectionHelpers
+
+open System
+open System.Reflection
+open System.Linq.Expressions
+
+open Microsoft.FSharp.Reflection
+open Microsoft.FSharp.Quotations
+
+let returnNotUnitSecond (_: 'a) (v2: 'b) = v2
+let sequenceExprs (expr1: Expr) (expr2: Expr) =
+    let meth = (getMethod <@ returnNotUnitSecond X X @>).MakeGenericMethod([|expr1.Type; expr2.Type|])
+    Expr.Call(meth, [expr1; expr2])
+
+let makeSingleArgWalkExpr (mtype: Type) (path: string list) dispatchOnType : Expr =
+    let arg = Var("x", mtype, false)
+    let useArg = Expr.Var(arg)
+    let contents = dispatchOnType mtype path useArg
+    Expr.Lambda(arg, contents)
+
+let genFieldWalker (instance1: Expr) (instance2: Expr) (field: PropertyInfo) (path: string list) dispatchOnType : Expr = 
+    let p1val = Expr.PropertyGet(instance1, field) 
+    let p2val = Expr.PropertyGet(instance2, field)
+    dispatchOnType field.PropertyType path p1val p2val
+
+let genRecordWalker (rtype: Type) (path: string list) (instance1: Expr) (instance2: Expr) dispatchOnType : Expr =  
+    FSharpType.GetRecordFields(rtype) 
+    |> Array.toList
+    |> List.map (fun field -> field.Name :: path, field)
+    |> List.map (fun (fpath, field) -> genFieldWalker instance1 instance2 field fpath dispatchOnType)
+    |> List.reduce (fun e1 e2 ->  sequenceExprs e1 e2) 
+
+let callFunAndCont (f: obj -> obj -> string list -> unit) (path: string list) (instance1: Expr) (instance2: Expr) (next: Expr) : Expr = 
+    let compExpr = <@@ f %%instance1 %%instance2 path @@>
+    sequenceExprs compExpr next
+
+let rec dispatchOnType (f: obj -> obj -> string list -> unit) (mtype: Type) (path: string list) (instance1: Expr) (instance2: Expr) : Expr = 
+    match mtype with 
+//   | IsOptionType _ -> walkOption mtype path instance1 instance2 dispatchOnType
+    | _ when FSharpType.IsRecord mtype -> 
+                let walker = genRecordWalker mtype path instance1 instance2 (dispatchOnType f)
+                callFunAndCont f path instance1 instance2 walker
+                //genRecordWalker mtype path instance1 instance2 (dispatchOnType f)
+//    | _ when FSharpType.IsUnion mtype  -> genUnionCopier rcs mtype funcs path instance
+//    | _ when mtype.IsValueType || mtype = typeof<String> -> genSimpleValueCopy rcs mtype funcs path instance 
+//    | _ when mtype.IsArray -> genArrayCopier rcs mtype funcs path instance
+//    | IsMapType _ -> genMapCopier rcs mtype funcs path instance
+//    | _ when FSharpType.IsTuple mtype -> genTupleCopyFunExpr rcs mtype funcs path instance
+//    | _ when mtype = typeof<obj> -> instance 
+//    | _ when rcs.FailOnUnsupportedType -> failwithf "Type not supported in record cloning: %s" mtype.FullName
+//    | _ -> instance
