00261206c907fa6a07fd6ade9b4c862737591e5e
Rick Minerich <richard.minerich@gmail.com>
2017-06-07 18:25:57 -0400

    Code cleanup.

--- src/Fungible/Walker.fs
+++ src/Fungible/Walker.fs
@@ -12,11 +12,9 @@ open System.Linq.Expressions
 open Microsoft.FSharp.Reflection
 open Microsoft.FSharp.Quotations
 
-// Note: Sequence N Exprs for speedup
-let returnNotUnitSecond (_: 'a) (v2: 'b) = v2
+
 let sequenceExprs (expr1: Expr) (expr2: Expr) =
-    let meth = (getMethod <@ returnNotUnitSecond X X @>).MakeGenericMethod([|expr1.Type; expr2.Type|])
-    Expr.Call(meth, [expr1; expr2])
+    Expr.Sequential(expr1, expr2)
 
 let sequenceLambda (t1: Type) (t2: Type)=
     let instance1Var = Var("instance1", t1, false)
@@ -24,20 +22,6 @@ let sequenceLambda (t1: Type) (t2: Type)=
     let instance2Var = Var("instance2", t2, false)
     let instance2Arg = Expr.Var(instance2Var)
     Expr.Lambda(instance1Var, Expr.Lambda(instance2Var, sequenceExprs instance1Arg instance2Arg))
-
-//    /// Compare Application
-//    let returnNotUnitSecond (_: 'a) (v2: 'b) = v2
-//    let inline applyCompare (prmType: Type) (name: string list) prm1 prm2 compFunExpr = 
-//        let inputs = Expr.NewTuple [Expr.Value (List.rev name); prm1; prm2]
-//        let app = Expr.Application(compFunExpr, inputs)
-//        let meth = (getMethod <@ returnNotUnitSecond X X @>).MakeGenericMethod([|typeof<unit>; prmType|])
-//        Expr.Call(meth, [app; prm2])
-
-//let makeSingleArgWalkExpr (mtype: Type) (path: string list) dispatchOnType : Expr =
-//    let arg = Var("x", mtype, false)
-//    let useArg = Expr.Var(arg)
-//    let contents = dispatchOnType mtype path useArg
-//    Expr.Lambda(arg, contents)
 
 let genFieldWalker (instance1: Expr) (instance2: Expr) (field: PropertyInfo) (path: string list) dispatchOnType : Expr = 
     let p1val = Expr.PropertyGet(instance1, field) 
@@ -50,7 +34,6 @@ let genRecordWalker (rtype: Type) (path: string list) (instance1: Expr) (instanc
     |> List.map (fun field -> field.Name :: path, field)
     |> List.map (fun (fpath, field) -> genFieldWalker instance1 instance2 field fpath dispatchOnType)
     |> List.reduce (fun e1 e2 ->  sequenceExprs e1 e2) 
-//    |> List.reduce (fun e1 e2 -> e1) 
 
 let makeFuncionCall (f: obj -> obj -> string list -> unit) (path: string list) (instance1: Expr) (instance2: Expr) =
     let objExpr1 = Expr.Coerce(instance1, typeof<obj>)
@@ -79,7 +62,7 @@ let rec dispatchOnType (f: obj -> obj -> string list -> unit) (mtype: Type) (pat
 //    | _ -> instance      
       | _ -> failwithf "Unexpected type: %s" (mtype.ToString())
 
-let makeSingleWalker (f: obj -> obj -> string list -> unit) (mtype: Type) (path: string list) : Expr =
+let makeWalkerLambdaExpr (f: obj -> obj -> string list -> unit) (mtype: Type) (path: string list) : Expr =
     let arg1 = Var("x", mtype, false)
     let useArg1 = Expr.Var(arg1)
     let arg2 = Var("y", mtype, false)
@@ -91,15 +74,8 @@ open FSharp.Quotations.Evaluator
 
 let generateWalker<'T> (f: obj -> obj -> string list -> unit) : ('T -> 'T -> unit) =
     let baseType = typeof<'T>
-
-    //let contents = dispatchOnType f baseType [] instance1Arg instance2Arg
-    let contents = makeSingleWalker f baseType []
-
+    let contents = makeWalkerLambdaExpr f baseType []
     let castExpr : Expr<'T -> 'T -> unit> = contents |> Expr.Cast
     castExpr.Compile()
-//    <@ fun (x: 'T) (y: 'T) -> () @>.Compile()
-       
-//    let linqExpr = castExpr |> toLinq2<'T,'T,unit>
-//    let compiledExpr = linqExpr.Compile()
-//    fun (i1: 'T) (i2 : 'T) -> compiledExpr.Invoke(i1, i2)
+
 
