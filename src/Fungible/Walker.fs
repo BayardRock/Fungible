0da029d3b967702967d4e73632ca2f4895d7ad83
Rick Minerich <richard.minerich@gmail.com>
2017-08-17 15:57:22 -0400

    FUN-1: Now with Union Types Support for Walker

--- src/Fungible/Walker.fs
+++ src/Fungible/Walker.fs
@@ -20,6 +20,17 @@ type WalkerSettings =
 
 type InputFunction = obj -> obj -> string list -> unit
 
+let compareArrays (a1: obj []) (a2: obj []) =   
+    let mutable idx1 = 0
+    let mutable idx2 = 0
+    while idx1 < a1.Length && idx2 < a2.Length do
+        // f()
+    ()
+
+let getTypedException (utype: Type) (message: string) =
+    let m = (getMethod <@ failwith message @>).MakeGenericMethod([|utype|])
+    Expr.Call(m, [ <@ message @> ])
+
 let sequenceExprs (expr1: Expr) (expr2: Expr) =
     Expr.Sequential(expr1, expr2)
 
@@ -50,6 +61,26 @@ let genPOCOWalker (ctype: Type) (path: string list) (instance1: Expr) (instance2
     |> List.map (fun (fpath, field) -> genFieldWalker instance1 instance2 field fpath dispatchOnType)
     |> List.reduce (fun e1 e2 ->  sequenceExprs e1 e2) 
 
+// NOTE: Will only walk if the union cases are the same
+let genUnionWalker (utype: Type) (path: string list) (instance1: Expr) (instance2: Expr) dispatchOnType : Expr =
+    let genCaseTest case =
+        let test1 = Expr.UnionCaseTest(instance1, case)
+        let test2 = Expr.UnionCaseTest(instance2, case)
+        <@@ (%%test1) && (%%test2) @@>
+
+    let genIf ifCase thenCase elseCase = Expr.IfThenElse(ifCase, thenCase, elseCase) 
+
+    let makeCaseWalker (ci: UnionCaseInfo) = 
+        ci.GetFields()
+        |> Array.map (fun field -> genFieldWalker instance1 instance2 field (field.Name :: path) dispatchOnType)
+        |> Array.reduce (fun e1 e2 -> sequenceExprs e1 e2)  
+
+    let doNothing() = ()
+
+    FSharpType.GetUnionCases utype  
+    |> Array.map (fun case -> genIf (genCaseTest case) (makeCaseWalker case))
+    |> Array.foldBack (fun iff st -> iff st) <| <@@ doNothing() @@>
+
 let makeFuncionCall (fexpr: Expr) (path: string list) (instance1: Expr) (instance2: Expr) =
     let objExpr1 = Expr.Coerce(instance1, typeof<obj>)
     let objExpr2 = Expr.Coerce(instance2, typeof<obj>)
@@ -61,6 +92,9 @@ let callFunAndCont (fexpr: Expr) (path: string list) (instance1: Expr) (instance
 
 let rec dispatchOnType (settings: WalkerSettings) (fexpr: Expr) (mtype: Type) (path: string list) (instance1: Expr) (instance2: Expr) : Expr = 
     match mtype with 
+    | _ when FSharpType.IsUnion mtype ->
+                let walker = genUnionWalker mtype path instance1 instance2 (dispatchOnType settings fexpr)
+                callFunAndCont fexpr path instance1 instance2 walker
     | _ when FSharpType.IsRecord mtype -> 
                 let walker = genRecordWalker mtype path instance1 instance2 (dispatchOnType settings fexpr)
                 callFunAndCont fexpr path instance1 instance2 walker
@@ -82,7 +116,7 @@ let makeWalkerLambdaExpr (settings: WalkerSettings) (ftype:Type) (mtype: Type) (
 
 open FSharp.Quotations.Evaluator
 
-let generateWalker<'T> (settings: WalkerSettings) : ((obj -> obj -> string list -> unit) -> 'T -> 'T -> unit) =
+let generateWalker<'T> (settings: WalkerSettings) : (InputFunction -> 'T -> 'T -> unit) =
     let baseType = typeof<'T>
     let funType = typeof<InputFunction>
     let contents = makeWalkerLambdaExpr settings funType baseType []
--- tests/Fungible.Tests/WalkerTests.fs
+++ tests/Fungible.Tests/WalkerTests.fs
@@ -41,17 +41,90 @@ let ``walker should be able to visit all members of a simple POCO type`` () =
 
 type TestSetProp (String: string, Integer: int, Float: float) =    
     member t.String = String
-    member t.Intger = Integer
+    member t.Integer = Integer
     member t.Float = Float
     member t.SetProp with set v = () 
 
 [<Fact>]
 let ``walker shouldn't expode on set only properties`` () =
     let output = ResizeArray<string>()
-    let walker (e1: obj) (e2: obj) (name: string list) =
+    let walker (e1: obj) (e2: obj) (name: string list) =        
         output.Add(sprintf "%A %A %A" e1 e2 name)
         
     let settings = { WalkerSettings.Default with CompareCSharpProperties = true }
     let walkerFun = generateWalker<TestSetProp> settings walker
     walkerFun (TestSetProp("Str1",1,1.0)) (TestSetProp("Str2",2,2.0))
     Assert.Equal(4, output.Count)
+
+type TestUnion = 
+    | String of string
+    | Integer of int
+    | Float of float
+
+type TestUnionRecord = 
+    {
+       TestUnion: TestUnion
+    }
+
+[<Fact>]
+let ``walker should compare the same basic union case properly`` () =
+    let output = ResizeArray<string>()
+    let walker (e1: obj) (e2: obj) (name: string list) =        
+        output.Add(sprintf "%A %A %A" e1 e2 name)
+        
+    let settings = { WalkerSettings.Default with CompareCSharpProperties = true }
+    let walkerFun = generateWalker<TestUnionRecord> settings walker
+    walkerFun ({ TestUnion = String "one"}) ({ TestUnion = String "two"})
+    Assert.Equal(3, output.Count) // one for the root, one for the member, one for inside
+
+[<Fact>]
+let ``walker shouldnt compare the same basic union case properly if different`` () =
+    let output = ResizeArray<string>()
+    let walker (e1: obj) (e2: obj) (name: string list) =        
+        output.Add(sprintf "%A %A %A" e1 e2 name)
+        
+    let settings = { WalkerSettings.Default with CompareCSharpProperties = true }
+    let walkerFun = generateWalker<TestUnionRecord> settings walker
+    walkerFun ({ TestUnion = String "one"}) ({ TestUnion = Integer 2 })
+    Assert.Equal(2, output.Count) // one for the root, one for the member
+
+type TestUnion2 = 
+    | EmbedFields of String: string * Integer: int * Float: float
+    | EmbedRec    of FlatRecord
+
+[<Fact>]
+let ``walker should compare the same union case with members properly`` () =
+    let output = ResizeArray<string>()
+    let walker (e1: obj) (e2: obj) (name: string list) =        
+        output.Add(sprintf "%A %A %A" e1 e2 name)
+        
+    let settings = { WalkerSettings.Default with CompareCSharpProperties = true }
+    let walkerFun = generateWalker<TestUnion2> settings walker
+    walkerFun (EmbedFields("one", 1, 1.0)) (EmbedFields("two", 2, 2.0))
+    Assert.Equal(4, output.Count) // one for the root, one for each member
+
+[<Fact>]
+let ``walker should compare the same union case with records properly`` () =
+    let output = ResizeArray<string>()
+    let walker (e1: obj) (e2: obj) (name: string list) =        
+        output.Add(sprintf "%A %A %A" e1 e2 name)
+        
+    let settings = { WalkerSettings.Default with CompareCSharpProperties = true }
+    let walkerFun = generateWalker<TestUnion2> settings walker
+    walkerFun (EmbedRec {String ="one"; Integer = 1; Float = 1.0}) (EmbedRec {String ="two"; Integer = 2; Float = 2.0}) 
+    Assert.Equal(5, output.Count) // one for the root, one for each member (plus mystery "Item")
+
+[<Fact>]
+let ``walker shouldn't compare the same union case with different fancy cases properly`` () =
+    let output = ResizeArray<string>()
+    let walker (e1: obj) (e2: obj) (name: string list) =        
+        output.Add(sprintf "%A %A %A" e1 e2 name)
+        
+    let settings = { WalkerSettings.Default with CompareCSharpProperties = true }
+    let walkerFun = generateWalker<TestUnion2> settings walker
+    walkerFun (EmbedRec {String ="one"; Integer = 1; Float = 1.0}) (EmbedFields("two", 2, 2.0))
+    Assert.Equal(1, output.Count) // one for the root
+
+    output.Clear()
+    walkerFun (EmbedFields("two", 2, 2.0)) (EmbedRec {String ="one"; Integer = 1; Float = 1.0})
+    Assert.Equal(1, output.Count) // one for the root
